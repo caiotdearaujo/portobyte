b3d2736fb00435e72b9449b2a7e062d18c95d277
Caio de Araújo <caiotdearaujo@gmail.com>
2024-04-09 20:06:39 -0300

    Aula 1 adicionada

---
+++ 2024.1/python/aula1/código.py
@@ -0,0 +1,43 @@
+# Isto é um comentário
+# Cada linha precisa
+# De um hashtag
+
+"""
+Isto é uma docstring
+Ela não necessita de uma declaração
+por linha, como o comentário
+"""
+
+print("Hello, world")
+
+print("Linha 1")
+print("Linha 2")
+
+print("Linha 1",end="-")
+print("Linha 2",end="-")
+
+print("Olá", "mundo")
+
+print("Olá", "mundo", sep="-")
+
+nome = "Abenilzo"
+print(nome)
+
+idade_do_cachorro = 11
+print("O cachorro tem", idade_do_cachorro, "anos")
+
+nome_do_aluno = "Matheus"        # str
+idade_do_aluno = 16              # int
+altura_do_aluno_em_metros = 1.80 # float
+aluno_aprovado = True            # bool
+
+n1 = n2 = 10
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+modulo = n1 % n2
+exponenciacao = n1 ** n2
+
+print(soma, subtracao, multiplicacao, divisao, modulo, exponenciacao)
